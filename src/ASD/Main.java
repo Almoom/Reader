c333170647a803675605f7f7bc4c7db0c3c91138
Almoom <32739874+Almoom@users.noreply.github.com>
2017-12-28 18:59:20 +0500

    Initial commit

---
+++ src/ASD/Main.java
@@ -0,0 +1,23 @@
+package ASD;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream("1.txt"), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
